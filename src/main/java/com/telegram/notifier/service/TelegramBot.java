06a012a34f1ca149e238202a615d707f48399d25
Alina <alina.meller.98@mail.ru>
2023-12-01 17:05:35 +0300

    Initial commit

---
+++ src/main/java/com/telegram/notifier/service/TelegramBot.java
@@ -0,0 +1,23 @@
+package com.telegram.notifier.service;
+
+import org.jvnet.hk2.annotations.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
